5a2f2ac657453866c1a434643c28dc9d586e7373
kkirby16 <kkirby16@my.whitworth.edu>
2021-08-03 13:43:19 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -2,9 +2,9 @@
 
 import React from "react";
 
-const MovieReviews = (props) => (
+const MovieReviews = ({ reviews }) => (
   <div className="review-list">
-    {props.reviews.map((review) => (
+    {reviews.map((review) => (
       <div className="review">
         <h1>{review.display_title}</h1>
         <p>{review.critics_pick}</p>
